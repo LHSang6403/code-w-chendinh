2c92cfdf474cb50e512b85a54e1f23e30c62d599
LHSang <lhsang64.contact@gmail.com>
2024-03-16 00:45:39 +0700

    fixed pb

--- src/app/HelloMama/thuvien/Components/ImagesCarousel.tsx
+++ src/app/HelloMama/thuvien/Components/ImagesCarousel.tsx
@@ -14,23 +14,26 @@ export function ImagesCarousel({ items }: { items: ImageItemType[] }) {
       opts={{
         align: "center",
       }}
-      className="w-full"
+      className="h-fit w-full pb-0"
     >
-      <CarouselContent>
+      <CarouselContent className="pb-0">
         {items.map((item, index) => (
-          <CarouselItem key={index} className="basis-1/3 lg:basis-1/2 pb-0">
+          <CarouselItem
+            key={index}
+            className="basis-1/3 pb-0 lg:basis-1/2"
+          >
             <div className="flex h-fit w-full flex-col items-center justify-start gap-2">
               <div className="max-h-[320px] w-full">
                 <Image
                   alt="Thư viện ảnh"
                   src={item.image}
                   width={934}
-                  height={934}
+                  height={670}
                   quality={100}
                 />
               </div>
               {item.description !== "" && (
-                <p className="line-clamp-2 lg:line-clamp-4 h-fit w-full overflow-ellipsis px-2 text-center font-light">
+                <p className="line-clamp-2 h-fit w-full overflow-ellipsis px-2 text-center font-light lg:line-clamp-4">
                   {item.description}
                 </p>
               )}
