a6d32a19d48e9ad9aac97e508687e4871e46057a
LHSang <lhsang64.contact@gmail.com>
2024-03-21 15:13:36 +0700

    fix ui cart selected

--- src/app/HelloMama/daili/Components/SearchListItem.tsx
+++ src/app/HelloMama/daili/Components/SearchListItem.tsx
@@ -1,23 +1,42 @@
 import { SearchListItemProps } from "@utils/types";
+import { useDaiLiSelect } from "@/zustand/useDaiLiSelect";
 
 export default function SearchListItem({ row }: { row: SearchListItemProps }) {
+  const { selectedDaiLi } = useDaiLiSelect();
+
   return (
-    <li className="group rounded-lg bg-gradient-to-r from-[#CD9F2D] via-[#F7EF8A] to-[#EDC967] p-[1px] hover:cursor-pointer hover:bg-gradient-to-b hover:from-[#1E588F] hover:via-[#0E7BB8] hover:to-[#0E7BB8]">
-      <div className="h-16 w-72 rounded-lg bg-white px-2 py-1 group-hover:bg-gradient-to-b group-hover:from-[#1E588F] group-hover:via-[#0E7BB8] group-hover:to-[#0E7BB8] xl:w-full sm:h-fit ">
-        <div className="line-clamp-1 overflow-ellipsis text-sm text-black group-hover:text-white">
+    <li className="rounded-lg bg-gradient-to-r from-[#CD9F2D] via-[#F7EF8A] to-[#EDC967] p-[1px] hover:cursor-pointer hover:bg-gradient-to-b hover:from-[#1E588F] hover:via-[#0E7BB8] hover:to-[#0E7BB8]">
+      <div
+        className={`h-16 w-72 rounded-lg bg-white px-2 py-1 xl:w-full sm:h-fit ${
+          selectedDaiLi.name === row.name
+            ? "bg-gradient-to-b from-[#1E588F] via-[#0E7BB8] to-[#0E7BB8]"
+            : ""
+        }`}
+      >
+        <div
+          className={`line-clamp-1 overflow-ellipsis text-sm text-black ${
+            selectedDaiLi.name === row.name ? "text-white" : ""
+          }`}
+        >
           {row.name}
         </div>
-        <div className="line-clamp-1 overflow-ellipsis text-[14px] text-[#1c1c1c] group-hover:text-white">
+        <div
+          className={`line-clamp-1 overflow-ellipsis text-[14px] text-[#1c1c1c] ${
+            selectedDaiLi.name === row.name ? "text-white" : ""
+          }`}
+        >
           {row.address}, {row.district}, {row.province}
         </div>
-        <div className="flex flex-row  items-center gap-1 text-[14px]">
+        <div className="flex flex-row items-center gap-1 text-[14px]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            className="h-3 w-3 group-hover:text-white"
+            className={`h-3 w-3 ${
+              selectedDaiLi.name === row.name ? "text-white" : ""
+            }`}
           >
             <path
               stroke-linecap="round"
@@ -25,7 +44,11 @@ export default function SearchListItem({ row }: { row: SearchListItemProps }) {
               d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
             />
           </svg>
-          <span className="line-clamp-1 overflow-ellipsis text-[#1c1c1c] group-hover:text-white">
+          <span
+            className={`line-clamp-1 overflow-ellipsis text-[#1c1c1c] ${
+              selectedDaiLi.name === row.name ? "text-white" : ""
+            }`}
+          >
             {row.phones.join(" - ")}
           </span>
         </div>
