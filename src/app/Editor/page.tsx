fe7cca48ef36644d69bbde78900656b54faadfe8
LHSang <lhsang64.contact@gmail.com>
2024-03-22 22:09:32 +0700

    fixed novel

--- src/app/Editor/page.tsx
+++ src/app/Editor/page.tsx
@@ -1,10 +1,9 @@
-"use client";
-
 import { Editor } from "novel";
 
 export default function page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Novel hehe
       <Editor />
     </main>
   );
