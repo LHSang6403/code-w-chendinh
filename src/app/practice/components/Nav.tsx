b27362a7bbd8f5120314482156e1e316d3181171
LHSang <lhsang64.contact@gmail.com>
2024-04-24 17:41:26 +0700

    update main

--- src/app/practice/components/Nav.tsx
+++ src/app/practice/components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="flex w-full flex-row gap-4 overflow-x-auto font-medium sm:text-sm">
+    <div className="flex w-fit flex-row gap-4 overflow-x-auto font-medium sm:text-sm">
       <Link
         href="#"
         className="w-fit text-nowrap border-b-[4px] px-1.5 sm:px-0"
