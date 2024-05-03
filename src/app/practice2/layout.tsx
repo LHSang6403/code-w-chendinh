52e1de4fa0108466f66d3300e07956256f59eec1
LHSang <lhsang64.contact@gmail.com>
2024-05-03 17:52:26 +0700

    doing new practice

--- src/app/practice2/layout.tsx
+++ src/app/practice2/layout.tsx
@@ -2,7 +2,7 @@ import Header from "./Components/Header";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-auto min-h-screen w-full flex-col items-center bg-black px-10 xl:px-6 sm:px-4">
+    <div className="flex h-auto min-h-screen w-full flex-col items-center bg-[#05010D] px-20 xl:px-10 sm:px-4">
       <Header />
       {children}
     </div>
