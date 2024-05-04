e323119729d0c2a9bdbe38efb12608586b4afd1e
LHSang <lhsang64.contact@gmail.com>
2024-05-04 21:53:19 +0700

    fixed ios

--- src/app/practice2/layout.tsx
+++ src/app/practice2/layout.tsx
@@ -3,7 +3,7 @@ import Header from "./Components/Header";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="w-full bg-[#05010D]">
+    <main className="w-full !appearance-none bg-[#05010D]">
       <div className="flex h-auto min-h-screen w-full flex-col items-center overflow-hidden px-20 xl:px-6 sm:px-4">
         <Header />
         {children}
